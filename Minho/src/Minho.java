684063d3926165426941115e9a9b8b9679d3f23b
choim <choim@192.168.76.66>
2024-11-21 21:55:33 +0900

    동한아 안녕

--- Minho/src/Minho.java
+++ Minho/src/Minho.java
@@ -4,6 +4,8 @@ public class Minho {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
+		System.out.println("깃허브 제발제발 임동한");
+		System.out.println("동한아 축하한다");
 	}
 
 }
