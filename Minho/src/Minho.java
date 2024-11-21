63787dac2fecdf5521e9898a4d557f55e3de77a1
choim <choim@192.168.76.66>
2024-11-21 21:58:20 +0900

    최민호

--- Minho/src/Minho.java
+++ Minho/src/Minho.java
@@ -4,8 +4,7 @@ public class Minho {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
-		System.out.println("깃허브 제발제발 임동한");
-		System.out.println("동한아 축하한다");
+		System.out.println("깃허브 제발제발 임동한 최민호");
 	}
 
 }
